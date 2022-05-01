cee202bf9becd25c62681829cb316ab872ca97b2
ccugabinete <ccugabinete@gamil.com>
2022-05-01 12:43:37 -0300

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const gets = require('../controllers/getsController');
-const posts = require('../controllers/postController');
 const tele = require('../controllers/telemetriaController');
-const poly = require('../controllers/polylinhaController')
+const poly = require('../controllers/polylinhaController');
 
-/* GET home page. */
-router.get('/', gets.getAll);
-router.post('/', posts.getAll);
 router.post('/telemetria', tele.getAll);
 router.post('/polylinha', poly.getPoly);
 
